d9837ef29f3699d9000de07065698747627eaa78
alexbbf <alex.bbf@hotmail.com>
2022-07-18 07:36:20 -0400

    segundo commit

--- src/main/java/io/github/alexbbf/clientes/ClientesApplication.java
+++ src/main/java/io/github/alexbbf/clientes/ClientesApplication.java
@@ -13,14 +13,7 @@ import java.util.zip.CRC32;
 @SpringBootApplication
 public class ClientesApplication {
 
-    @Bean
-    public CommandLineRunner run(@Autowired ClienteRepository repository){
-       return args -> {
-           Cliente cliente =  Cliente.builder().cpf("00123764262").nome("Alex Bruno").build();
-           repository.save(cliente);
 
-       } ;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ClientesApplication.class, args);
